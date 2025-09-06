4ae3dcb15a0138b6805ccf8156a48b23e18a77f0
ayushkumar1991 <ayushkumr1991@gmail.com>
2025-09-06 18:16:24 +0530

    Evo2 model integrated

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 
 # ------------------------------
 # Request body schema
-
 # ------------------------------
 class VariantRequest(BaseModel):
     variant_position: int
